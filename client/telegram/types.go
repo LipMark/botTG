9f247caeb7170b466a2f296b42f6105515710f95
LipMark <144370265+LipMark@users.noreply.github.com>
2024-09-30 08:02:00 +0300

    fix types

--- client/telegram/types.go
+++ client/telegram/types.go
@@ -2,12 +2,26 @@ package telegram
 
 // structure of telegram API getUpdates chapter "getting updates"
 type Update struct {
-	ID      int    `json:"update_id"`
-	Message string `json:"message"`
+	ID      int              `json:"update_id"`
+	Message *IncomingMessage `json:"message"`
 }
 
 // for more info see "making requests" chapter of telegram API
 type UpdateResponse struct {
 	OK     bool     `json:"ok"`
 	Result []Update `json:"result"`
+}
+
+type IncomingMessage struct {
+	Text string `json:"text"`
+	From From   `json:"from"`
+	Chat Chat   `json:"chat"`
+}
+
+type From struct {
+	Username string `json:"username"`
+}
+
+type Chat struct {
+	ID int `json:"id"`
 }
--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,8 +1,54 @@
 package telegram
 
-import "TGBot/client/telegram"
+import (
+	"fmt"
+
+	"TGBot/client/telegram"
+	"TGBot/events"
+	"TGBot/storage"
+)
 
 type Dispatcher struct {
-	tg     *telegram.Client
-	offset int
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
+}
+
+func newDispatcher(client *telegram.Client, storage storage.Storage) *Dispatcher {
+	return &Dispatcher{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (d *Dispatcher) Fetch(limit int) ([]events.Event, error) {
+	update, err := d.tg.Updates(d.offset, limit)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch events %w", err)
+	}
+
+	fetchResult := make([]events.Event, 0, len(update))
+
+	for _, fetchedEvent := range update {
+		fetchResult = append(fetchResult, toEvent(fetchedEvent))
+	}
+}
+
+// func event is used to convert update into event
+func toEvent(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
 }
